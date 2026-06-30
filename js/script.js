39f3e5b80593f972794a381ebef519f44c3b50a2
Rogerio Curtio <rogeriocurtiogithub@gmail.com>
2026-06-29 21:12:09 -0300

    Atualização Estilo CSS. Atualização JavaScript

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 function calcular() {
     let val = window.document.getElementById('ival')
     let res = window.document.getElementById('resposta')
+
+      // Verifica se o campo está vazio
+    if (val.value.trim() === "") {
+        alert("Digite um número!")
+        val.focus()
+        return
+    }
+
     let vl = Number(val.value)
     if (vl % 2 == 0) {
-        res.innerHTML = `O Valor ${vl} <strong> é PAR </strong>`
+        res.innerHTML = `<p>O Valor ${vl} <strong> é PAR </strong>
+        </p>`
     } else {
-        res.innerHTML = `O Valor ${vl} é
-        <strong> ÍMPAR </strong>`
+        res.innerHTML = `<p>O Valor ${vl} é
+        <strong> ÍMPAR </strong></p>`
     }
 }
